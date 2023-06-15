1d4e3105f0c74b5afa47878ee343d9fc8474287a
wesley-azevedo <azevedosou@gmail.com>
2023-06-15 20:51:47 -0300

    subindo preenchimento de cep e demais informações

--- scripts.js
+++ scripts.js
@@ -13,6 +13,27 @@ fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
      
      console.log(estados)
      inputEstado.innerHTML = estados
-})
+    })
 }
 findEstados()
+
+function findByCep()  {
+  //pega o valor docep digitado pelo usuario através do id cep do HTML
+  let cepValue = cep.value
+  console.log(cepValue)
+
+  fetch(`http://viacep.com.br/ws/${cepValue}/json/`)
+  .then(response => response.json())
+  .then(data => {
+      preencherCampos(data)
+    
+  })
+}
+
+const preencherCampos = json => {
+  console.log(json)
+  logradouro.value = json.logradouro
+  numero.focus()
+  bairro.value = json.bairro
+
+}
